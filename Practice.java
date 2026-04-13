d4f2b03b53aa5ed0b10b21761bd8b0117d2ae845
prabhleenkaur-173 <prabhsaggu55@gmail.com>
2026-04-13 07:58:26 +0530

    Practice.java file

---
+++ Practice.java
@@ -0,0 +1,14 @@
+public class Practice {
+    public static void main(String[] args){
+        System.out.print("To find the difference :");
+        float a= 1234.55f;
+        float b= 124.675f;
+        //for float we have to add f after the value
+        // but in double we don't need to add f
+        float c = a+b;
+        float d = a-b;
+        System.out.println (c);
+        System.out.print(d);
+
+    }
+}
